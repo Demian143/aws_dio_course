621b1df1e3eb3498cec3d6d164911c5c92d6489c
Demian143 <patrickxavier140@gmail.com>
2022-11-24 18:47:53 -0300

    with index

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+// const AWS = require(aws-sdk);
+
+exports.handler = async (event, context) => {
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+
+    let responseBody = '';
+    let statusCode = 0;
+
+    // const {id, tableName, itemPrice} =  JSON.parse(event.body);
+
+    const params = {
+        TableName: "Item",
+        Item: {
+            id: 'item-01',
+            itemName: 'item01',
+            itemPrice: 15
+        }
+    }
+
+    try {
+
+        const data = await documentClient.put(params).promise();
+        responseBody = JSON.stringify(data.Items);
+        statusCode = 201;
+
+    } catch (err) {
+
+        responseBody = `Falha ao listar items ${err}`;
+        statusCode = 403;
+
+    }
+
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: responseBody
+    };
+
+    return response;
+}
