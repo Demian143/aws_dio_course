bd75db39b8ef7db12603e594059a7561b0c17be3
Demian143 <patrickxavier140@gmail.com>
2022-11-25 18:49:15 -0300

    re-arrumando a estrutura

---
+++ code/DioLiveListItem.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+// const AWS = require(aws-sdk);
+
+exports.handler = async (event, context) => {
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+
+    let responseBody = '';
+    let statusCode = 0;
+
+    // const {id, tableName, itemPrice} =  JSON.parse(event.body);
+
+    const params = {
+        TableName: "Item"
+    }
+
+    try {
+
+        const data = await documentClient.scan(params).promise();
+        responseBody = JSON.stringify(data.Items);
+        statusCode = 201;
+
+    } catch (err) {
+
+        responseBody = `Falha ao listar items ${err}`;
+        statusCode = 403;
+
+    }
+
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: responseBody
+    };
+
+    return response;
+}
